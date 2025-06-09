0fd6af27915bec3fe15f4d088a5b794262cd77b3
IgorSemed0 <semedoigor4@gmail.com>
2025-06-09 03:38:06 +0100

    feat: dashboard show

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,16 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
     use SoftDeletes;
 
-    protected $table = 'users';
     protected $fillable = [
         'vc_nome',
         'email',
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -33,7 +33,7 @@ export interface SharedData {
 
 export interface User {
     id: number;
-    name: string;
+    vc_nome: string;
     email: string;
     avatar?: string;
     email_verified_at: string | null;
